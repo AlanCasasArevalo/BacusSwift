30903a0e4d43c8f24e4eac7f63b4830217003e50
Unknown <alancasasarevalo@gmail.com>
2018-12-31 09:19:30 +0100

    [EVOLUTION]
    Model was created
    Constants file was created

---
+++ BacusSwift/Constants.swift
@@ -0,0 +1,55 @@
+//
+//  Constants.swift
+//  BacusSwift
+//
+//  Created by Alan Casas on 31/12/18.
+//  Copyright © 2018 Alan Casas. All rights reserved.
+//
+
+import Foundation
+
+let CONSTANTS = Constants()
+
+struct Constants {
+    let WINE_MODEL_CONSTANTS = WineModelConstants()
+}
+
+struct WineModelConstants {
+    let NO_RATING_NUMBER: Int = -1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
